8fdf3924ab9d67acb51749bace4891ea2eb593ff
Faishal Rachman <faishalr97@gmail.com>
2020-05-27 21:58:05 +0700

    dev(ui) : adding english version of body type

--- libs/body_shape.py
+++ libs/body_shape.py
@@ -7,11 +7,26 @@ import matplotlib.pyplot as plt
 import base64
 
 body_string = {
-    "H" : "Choose A-line skirts with ruffled and layered tops. Dresses that add definition to your bottom and necklines that add weight to the upper body. Sleeveless, strapless and sweetheart lines are your thing. Blazers, long jackets and capes add the much-needed drama here.",
-    "X" : "Dresses that cinch at the waist will be your best fit. V-necks or plunge necklines and sweetheart necklines help you flaunt your upper body. To show off your waistline, go with a belt at your natural waistline or just below the belly button. A-line dresses or similar cuts take care of working on the lower part of the body and of course, body-hugging dresses are your thing, because, why not?",
-    "O" : "Find clothes that suit your body type by looking to a-line or empire cut blouses and dresses. Wear printed dresses or patterned jackets that add a layer to shift the focus. Monochrome looks, dark colors, full or 3/4th sleeves dresses and flowy tops will help. You could also wear flared bottoms; palazzos etc. to create a balance. Also, since your shoulders are broad and you might already have a bigger bustline, ensure you wear the right bra.",
-    "A" : "Find clothes that suit your body type by looking to wide legged pants, A-line skirts or dresses with patterned or ruffled tops that add definition to the upper body. Skinny jeans with loose tops help create an hourglass illusion and high waist jeans show off the smallest part of your waist. Crop tops, sweetheart, v-necks, scoop or boat necks will balance your bottom out.",
-    "V" : "Since your hips are much narrower than your shoulders, pencil skirts or skinny jeans with any top will look great. V-neck lines work well and create an illusion of narrow shoulders, so this should be your go-to neckline."
+    "H" : {
+      "title" : "Rectangle",
+      "desc" : "Choose A-line skirts with ruffled and layered tops. Dresses that add definition to your bottom and necklines that add weight to the upper body. Sleeveless, strapless and sweetheart lines are your thing. Blazers, long jackets and capes add the much-needed drama here."
+    },
+    "X" : {
+      "title" : "Hourglass",
+      "desc" : "Dresses that cinch at the waist will be your best fit. V-necks or plunge necklines and sweetheart necklines help you flaunt your upper body. To show off your waistline, go with a belt at your natural waistline or just below the belly button. A-line dresses or similar cuts take care of working on the lower part of the body and of course, body-hugging dresses are your thing, because, why not?"
+    },
+    "O" : {
+      "title" : "Apple",
+      "desc" : "Find clothes that suit your body type by looking to a-line or empire cut blouses and dresses. Wear printed dresses or patterned jackets that add a layer to shift the focus. Monochrome looks, dark colors, full or 3/4th sleeves dresses and flowy tops will help. You could also wear flared bottoms; palazzos etc. to create a balance. Also, since your shoulders are broad and you might already have a bigger bustline, ensure you wear the right bra."
+    },
+    "A" : {
+      "title" : "Triangle",
+      "desc" : "Find clothes that suit your body type by looking to wide legged pants, A-line skirts or dresses with patterned or ruffled tops that add definition to the upper body. Skinny jeans with loose tops help create an hourglass illusion and high waist jeans show off the smallest part of your waist. Crop tops, sweetheart, v-necks, scoop or boat necks will balance your bottom out."
+    },
+    "V" : {
+      "title" : "Inverted Triangle",
+      "desc" : "Since your hips are much narrower than your shoulders, pencil skirts or skinny jeans with any top will look great. V-neck lines work well and create an illusion of narrow shoulders, so this should be your go-to neckline."
+    }
 }
 
 def is_equal(a, b, tol_rate):
@@ -117,13 +132,14 @@ def getShapeFromImage(file_path, MODEL):
   newByte.seek(0)
   encoded_string = base64.b64encode(newByte.getvalue()).decode("utf-8")
   
+  body_code = body_string[bentuk_badan]
   #Metadata
   output = {
     "bahu" : lebar_bahu,
     "pinggang": lebar_pinggang,
     "panggul": lebar_panggul,
-    "bentuk_badan" : bentuk_badan,
-    "saran" : body_string[bentuk_badan],
+    "bentuk_badan" : body_code["title"],
+    "saran" : body_code["desc"],
     "image_encoded" : encoded_string
   }
   #Return the body shape measurement (H, A, X, O, V)
